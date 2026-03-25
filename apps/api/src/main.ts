4e34b92fec88911aaa368efff2bc3632440df392
gbbelloponce <gbbelloponce@gmail.com>
2026-03-25 18:39:23 -0300

    fix: enable CORS; add .env vars

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: process.env.CORS_ORIGIN_URL ?? "http://localhost:3000",
+	});
 	await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
