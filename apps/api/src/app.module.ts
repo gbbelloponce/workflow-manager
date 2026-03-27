9f323be1b26b74be0324afbdab72a4303c28fda4
gbbelloponce <gbbelloponce@gmail.com>
2026-03-27 01:18:03 -0300

    fix(api): delete initial scaffold files

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from "@nestjs/common";
 import { TRPCModule } from "nestjs-trpc";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { EventsModule } from "./features/events/events.module";
 import { NotificationsModule } from "./features/notifications/notifications.module";
 import { TriggerModule } from "./features/trigger/trigger.module";
 import { WorkflowsModule } from "./features/workflows/workflows.module";
 import { PrismaModule } from "./shared/db/prisma.module";
-import { AppRouter } from "./shared/trpc/app.router";
 
 @Module({
 	imports: [
@@ -18,7 +15,5 @@ import { AppRouter } from "./shared/trpc/app.router";
 		EventsModule,
 		NotificationsModule,
 	],
-	controllers: [AppController],
-	providers: [AppService, AppRouter],
 })
 export class AppModule {}
